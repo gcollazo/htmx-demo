993a3dfb1dc9f4b42969569fa46ed46e43137b53
Giovanni Collazo <gcollazo@users.noreply.github.com>
2021-04-24 21:41:33 -0400

    Add port env var

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { fragment, layout } from "./page.js";
 import express from "express";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 const MIN = 0;
 const MAX = 10;
@@ -39,6 +40,6 @@ app.post("/sub", (req, res) => {
   res.send(fragment(state));
 });
 
-app.listen(3000, () => {
-  console.log("Listening on http://localhost:3000....");
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}....`);
 });
